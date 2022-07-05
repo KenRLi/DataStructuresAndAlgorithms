dd4604f4e86152ad9ca80ca4de2d7eda55f06c41
Ken Li <krli@ualberta.ca>
2022-07-04 20:35:17 -0600

    Initial tree and graph

---
+++ 1-Python/1-DataStructures/5-Trees/tree.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, value=None, parent=None, children=None):
+        self.value = value
+        self.parent = parent
+        self.children = children
+
+class Tree:
+    def ___init__(self, root=None):
+        self.root = root
